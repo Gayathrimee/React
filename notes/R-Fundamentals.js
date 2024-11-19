b2bb2592fa60c6ad4f6e2e57ac3559d6442277d6
Gayathri <gayathrikrishna3132003@gmail.com>
2024-11-19 16:32:49 +0530

    react

---
+++ notes/R-Fundamentals.js
@@ -0,0 +1,11 @@
+What is React? 
+React is an open source JS library for building user interfaces. 
+ - React is a JS Library 
+ - Building User Interfaces (it doesnt work on other aspects of our application like routing and http requests)
+React has a rich eco system and plays really well with other libraries and is more than capable of building enterprise scale web applications
+
+Why React?
+1. One of the most popular UI libraries and is well known in the job market
+2. React has a great community behind it 
+3. React has a component based architecture  - component make to reuse the code. 
+4. React is declarative       
